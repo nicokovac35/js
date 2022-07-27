9fa85c70c67f75fd011c34d31ce17b1e0988a43a
nico martinez <divnicokovacdiv@gmail.com>
2022-07-27 12:41:20 -0400

    first commit2

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
 
 
+let cantidadUF 
+let calculo
+let UF = 32.255
+
+do {
+    nota = prompt("el valor de la UF de hoy es " + UF)
+    cantidadUF = parseFloat(prompt("Ingrese su cantidad de UF")) //"5" "hola" NaN = not a number)
+    
+break
+} while(isNaN(cantidadUF * UF == calculo))
+    document.write(calculo) //UNICA condicion de verdad para que se termine es que ambas cond. sean falsas
+
+
