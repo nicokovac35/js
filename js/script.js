83d34c7e413809dcbd421db55fb32d28f94c9a27
nico martinez <divnicokovacdiv@gmail.com>
2022-07-27 14:03:31 -0400

    first commit3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+// Pagos en UF
 let cantidadUF 
 let calculo
 let UF = 32.255
@@ -11,5 +11,15 @@ do {
 break
 } while(isNaN(cantidadUF * UF == calculo))
     document.write(calculo) //UNICA condicion de verdad para que se termine es que ambas cond. sean falsas
+
+// total de productos
+
+let producto1 = 15900
+let producto2 = 18900
+
+
+let totalProductos = (producto1 + producto2)
+
+console.log(totalProductos)
 
 
