372d3b5f6ff234f8af1e5f5295f0d7e3400732c8
nico martinez <divnicokovacdiv@gmail.com>
2022-07-28 21:55:05 -0400

    first commit5

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-
+/*
 // Pagos en UF
-let cantidadUF 
+let cantidadUF
+ 
 let calculo
 let UF = 32.255
 
@@ -21,5 +22,41 @@ let producto2 = 18900
 let totalProductos = (producto1 + producto2)
 
 console.log(totalProductos)
+
+
+
+
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const d = new Date();
+let month = months[d.getMonth()];
+document.getElementById("demo").innerHTML = month;
+
+*/
+
+
+// Arrays  metodos y propiedades
+
+// declaracion de  array vacio y variable para determinar cantidad
+const listaNombres = [];
+let cantidad = 5;
+
+// empleo de do.. while para cargar nombres en el array por prompt()
+
+do{
+    let entrada = prompt("Ingresar Nombre");
+    listaNombres.push(entrada.toUpperCase());
+    console.log(listaNombres.length);
+
+}while (listaNombres.length  != cantidad)
+//Concatenamos un nuevo array de dos elementos
+
+const nuevaLista = listaNombres.concat(["ANA","EMA"]);
+
+//Salida con salto de linea usando join
+
+alert(nuevaLista.join("\n"));
+
+
 
 
